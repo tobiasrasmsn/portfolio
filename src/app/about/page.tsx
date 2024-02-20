06202a30923c1356677b712eda44a2e78d2be48d
tobiasrasmsn <tobiasrasmsn@protonmail.com>
2024-02-20 19:01:12 +0100

    Removed about page for now

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import Hero from "@/components/shared/Hero";
-
-export default function Home() {
-    return (
-        <main className="mx-[6vw]">
-            <Hero fullHeight={true} />
-        </main>
-    );
-}
